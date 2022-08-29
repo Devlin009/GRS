d377fc0845931842f473cd4f4118783485162314
devlin009 <bibekshrestha855@gmail.com>
2022-08-29 00:28:39 -0700

    first commit

---
+++ WebApplication2/Data/AddDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using WebApplication2.Models;
+
+namespace WebApplication2.Data
+{
+    public class AddDbContext : DbContext
+    {
+        public AddDbContext(DbContextOptions<AddDbContext> options) : base(options) { }
+        public DbSet<User> Users { get; set; }
+
+    }
+    
+}
